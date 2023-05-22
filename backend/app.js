af861c7c9b2865740e3fee79db7ebdb7d04559ed
TearsOprah <qmeshokp@mail.ru>
2023-05-22 14:57:18 +0300

    feat: Middleware для настройки заголовков CORS

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Middleware для настройки заголовков CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mesto.tearsoprah.nomoredomains.monster');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(requestLogger);
 
 app.use('/', rootRouter);
