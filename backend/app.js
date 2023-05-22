51b6510a5e5d5e4c28e20d21bfefbe8282cd925f
TearsOprah <qmeshokp@mail.ru>
2023-05-22 15:07:50 +0300

    feat: модуль CORS

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const NotFoundError = require('./errors/NotFound');
 const errorHandler = require('./errors/errorHandler');
 const rootRouter = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 
@@ -17,13 +18,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware для настройки заголовков CORS
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://mesto.tearsoprah.nomoredomains.monster');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+// использование модуля cors
+app.use(cors());
 
 app.use(requestLogger);
 
