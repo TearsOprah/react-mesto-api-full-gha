bfc20cd2e45ea47f1767de9af31451ddb7fa6748
TearsOprah <qmeshokp@mail.ru>
2023-05-23 09:31:02 +0300

    сообщение об ошибке mongo

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,16 @@ app.use(helmet());
 
 // подключение к базе данных
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
