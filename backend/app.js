29ca31b9eafdad927d5cb24b05e59dbd4c9b24a8
TearsOprah <qmeshokp@mail.ru>
2023-05-23 19:14:38 +0300

    feat: Краш-тест сервера

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/', rootRouter);
 
 app.use(errorLogger);
